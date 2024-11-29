8b3a3a8a21f5a6bff09aa0cf4810d6a2fd2c6d21
Mirian Ogata <mirianogata@gmail.com>
2024-11-29 17:04:40 -0300

    Add files via upload

---
+++ packages/core/src/evento/index.ts
@@ -0,0 +1,16 @@
+import Convidado from "./model/Convidado"
+import Evento from './model/Evento'
+
+import complementarConvidado from "./functions/complementarConvidado";
+import complementarEvento from "./functions/complementarEvento";
+import criarConvidadoVazio from "./functions/criarConvidadoVazio";
+import criarEventoVazio from "./functions/criarEventoVazio";
+
+export type{
+    Evento,
+    Convidado,
+    complementarConvidado,
+    complementarEvento,
+    criarConvidadoVazio,
+    criarEventoVazio,
+};
